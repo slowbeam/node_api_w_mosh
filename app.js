eddd5715ac6e68ed8ee043ad00942be2de37f4c2
slowbeam <sedwardscode@gmail.com>
2019-03-25 12:30:09 -0400

    custom event emitter class test

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const EventEmitter = require('events');
-const emitter = new EventEmitter();
+const Logger = require('./logger');
+const logger = new Logger();
 
 // Register a listener
-emitter.on('messageLogged', function() {
-    console.log('Listener called');
-})
+logger.on('messageLogged', (arg) => {
+    console.log('Listener called', arg);
+});
 
-// Raise an event
-emitter.emit('messageLogged');
+logger.log('message');
 
