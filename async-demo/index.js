617d72abb60379843d870c24601fa661063ae05c
slowbeam <sedwardscode@gmail.com>
2019-03-27 15:58:55 -0400

    completed async exercise

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,9 +1,26 @@
 console.log('Before');
-getUser(1)
-    .then(user => getRepositories(user.gitHubUsername))
-    .then(repos => getCommits(repos[0]))
-    .then(commits => console.log('Commits', commits))
-    .catch(err => console.log('Error', err.message));
+// Promise-based approach
+// getUser(1)
+//     .then(user => getRepositories(user.gitHubUsername))
+//     .then(repos => getCommits(repos[0]))
+//     .then(commits => console.log('Commits', commits))
+//     .catch(err => console.log('Error', err.message));
+
+// Async and Await approach
+async function displayCommits() {
+    try {
+        const user = await getUser(1);
+        const repos = await getRepositories(user.gitHubUsername);
+        const commits = await getCommits(repos[0]);
+        console.log(commits);
+    }
+    catch (err) {
+        console.log('Error', err.message);
+    }
+}
+displayCommits();
+
+
 console.log('After');
 
 // Callbacks
@@ -24,7 +41,7 @@ function getRepositories(username) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             console.log(`Getting repositories for ${username}...`);
-            resolve(['repo1', 'repo2', 'repo3'])
+            resolve(['repo1', 'repo2', 'repo3']);
         }, 2000);
     });   
 }
